686ca272abec805e52e490d8f60ffb472da57eea
entertthk <markkc2@hotmail.com>
2020-01-31 10:43:28 +0200

    eestiApi

---
+++ eestiAPI/Eesti.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eestiAPI
+{
+    public class Eesti
+    {
+        public string Name { get; set; }
+        public string Cioc { get; set; }
+        public string Capital { get; set; }
+        public string Region { get; set; }
+        public int Population { get; set; }
+
+        public List<string> topLevelDomain;
+        public List<Language> Languages { get; set; }
+
+        public class Language
+        {
+            public string Name { get; set; }
+        }
+
+    }
+
+}
---
+++ eestiAPI/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace eestiAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string categoryUrl = "https://restcountries.eu/rest/v2/name/eesti";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(categoryUrl);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                var info = JsonConvert.DeserializeObject<List<Eesti>>(response);
+                Console.WriteLine($"Name: {info[0].Name}");
+                Console.WriteLine($"Cioc: {info[0].Cioc}");
+                Console.WriteLine($"Capital: {info[0].Capital}");
+                Console.WriteLine($"Region: {info[0].Region}");
+                Console.WriteLine($"Population: {info[0].Population}");
+                Console.WriteLine($"Domain: {info[0].topLevelDomain[0]}");
+                Console.WriteLine($"Language: {info[0].Languages[0].Name}");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
